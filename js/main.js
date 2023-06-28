0d7726d2802734e80b04b5ae8fa95cd144c7b3e7
Minh Huynh <minhnhalang451@gmail.com>
2023-06-28 12:17:44 +0700

    Updated

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ overlay.addEventListener("click", () => {
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
 });
+
+// DARK MODE
+// Lấy ra button và body của trang
+const darkModeBtn = document.getElementById('dark-mode-btn');
+const body = document.body;
+
+// Sử dụng event listener để nghe sự kiện click vào button
+darkModeBtn.addEventListener('click', function() {
+  // Toggle class 'dark-mode' cho body
+  body.classList.toggle('dark-mode');
+});
