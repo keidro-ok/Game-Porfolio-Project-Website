ae70f2862bfe537a77023fba543dc128a088f6ea
Minh Huynh <minhnhalang451@gmail.com>
2023-07-01 18:49:19 +0700

    Updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// Change AVATAR
+
+const avatar = document.getElementById('avatar');
+const inputAvatar = document.createElement('input');
+inputAvatar.type = 'file';
+
+avatar.addEventListener('click', () => {
+  inputAvatar.click();
+});
+
+inputAvatar.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file && file.type.startsWith('image/')) {
+    const imageURL = URL.createObjectURL(file);
+    avatar.src = imageURL;
+
+    // Lưu URL vào LocalStorage
+    localStorage.setItem('avatarImage', imageURL);
+  }
+});
+
+// Kiểm tra xem đã có ảnh đại diện trong LocalStorage hay không
+const savedAvatarImage = localStorage.getItem('avatarImage');
+if (savedAvatarImage) {
+  // Hiển thị ảnh đại diện đã lưu
+  avatar.src = savedAvatarImage;
+}
+
+
+
 // Menu
 const menu = document.querySelector(".navbar__links");
 const menuButton = document.querySelector(".navbar__icons");
